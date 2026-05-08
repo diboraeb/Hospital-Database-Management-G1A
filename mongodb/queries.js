974ac6d32f7e17975c400dbf9f18480a96bda883
abelbir210 <abelbir210@gmail.com>
2026-05-08 22:33:05 +0300

    Create queries.js

---
+++ mongodb/queries.js
@@ -0,0 +1,6 @@
+db.createCollection("patients");
+db.createCollection("doctors");
+db.createCollection("appointments");
+db.createCollection("lab_orders");
+db.createCollection("payments");
+print("Hospital Database Collections Initalized Successfully.");
