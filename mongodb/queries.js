bc8b414461a38775dc6f94f353e67adc5529438a
abelbir210 <abelbir210@gmail.com>
2026-05-15 07:41:58 +0300

    queries.js

--- mongodb/queries.js
+++ mongodb/queries.js
@@ -1,1 +1,28 @@
-
+db.createCollection("patients");
+db.createCollection("staff");
+db.createCollection("inventory");
+print("Hospital Database COllection Initialized Successfully.");
+db.staff.find({
+  "role": "Nurse",
+  "shift": "Night",
+  "department.dept_name": "Internal Medicine"
+});
+db.patients.aggregate([
+  { $unwind: "$billings" },
+  { $match: { "billings.status": "Unpaid" } },
+  { $group: { _id: null, totalOwed: { $sum: "billings.amount"} } }
+  ]);
+db.inventory.updateOne(
+  { "med_name": "Amoxicillin" },
+  { $inc: { "stock_qty": -1 } }
+  );
+db.patients.find(
+  { "first_name": "Abebe", "last_name": "Bikila" },
+  {
+    "first_name": 1,
+    "last_name": 1,
+    "vitals.emergency_contact: 1,
+    "vitals.emergency_phone: 1
+  }
+);
+  
