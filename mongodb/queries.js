04ce802a937b870b454ebc7e000340d8af13f85f
Elisheba-101 <abtd.0809@gmail.com>
2026-05-22 12:30:43 +0300

    Initialize Hospital Database and add queries

--- mongodb/queries.js
+++ mongodb/queries.js
@@ -1,21 +1,32 @@
+print("Initializing Hospital Database Collection.")
 db.createCollection("patients");
 db.createCollection("staff");
 db.createCollection("inventory");
-print("Hospital Database COllection Initialized Successfully.");
+print("Hospital Database COllection Initialized Successfully.\n");
+
+//Query A: Find Night shift Nurses in Internal Medicine
 db.staff.find({
   "role": "Nurse",
   "shift": "Night",
   "department.dept_name": "Internal Medicine"
 });
+
+//Query B: calculate total unpaid bills Hospital-Wide
 db.patients.aggregate([
   { $unwind: "$billings" },
   { $match: { "billings.status": "Unpaid" } },
   { $group: { _id: null, totalOwed: { $sum: "billings.amount"} } }
   ]);
+
+//Query C:Decrement Inventory Stock when Medication is Dispensed
 db.inventory.updateOne(
   { "med_name": "Amoxicillin" },
   { $inc: { "stock_qty": -1 } }
   );
+print("Stock updated. Verifying new stock:");
+db.inventory.findOne({"med_name": "Amoxicillin"}, { "stock_qty": 1 });
+
+//Query D:Get patient emergency contact details
 db.patients.find(
   { "first_name": "Abebe", "last_name": "Bikila" },
   {
